8dd2516eb1e9a02b53775b98fb50029b5cc19893
Tolibaevrinat <webtolibaevrinat@gmail.com>
2025-02-21 14:33:34 +0500

    21-20-2025 14-33

---
+++ src/pages/EditProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './EditProfile.module.scss'
+
+const EditProfile = () => {
+  return (
+    <section className={styles.root}></section>
+  )
+}
+
+export default EditProfile
